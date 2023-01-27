5f15d0a8bee8e1a18a29c2f35b120cc5837c83e3
Luciano Popó <lucianopopo@gmail.com>
2023-01-27 10:53:16 -0300

    Atualização de config do git para azure

---
+++ src/main/java/br/com/lucianopopo/azuretest/TesteAzureApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucianopopo.azuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAzureApplication.class, args);
+	}
+
+}
